4f84ea96e5925660018b97d61bc4feaee6491abc
calincimpeanfsega <115559117+calincimpeanfsega@users.noreply.github.com>
2024-07-11 16:17:26 +0300

    changes to the project structure

---
+++ music-library/backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import albumRoutes from './routes/albums.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
+};
+
+mongoose.connect(process.env.MONGO_URI, mongooseOptions)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('MongoDB connection error:', err));
+
+app.use('/api/albums', albumRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- music-library/frontend/src/App.js
+++ music-library/frontend/src/App.js
@@ -2,19 +2,15 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ArtistList from './components/ArtistList';
-import AlbumList from './components/AlbumList';
 import Homepage from './components/HomePage';
 
 const App = () => {
     return (
         <Router>
             <div>
-                <h1>Music Library</h1>
+                {/* <h1>Music Library</h1> */}
                 <Routes>
-                    <Route path="/artists" element={<ArtistList />} />
-                    <Route path="/albums" element={<AlbumList />} />
-                    <Route path="/home" element={<Homepage />} />
+                    <Route path="/" element={<Homepage />} />
                 </Routes>
             </div>
         </Router>
